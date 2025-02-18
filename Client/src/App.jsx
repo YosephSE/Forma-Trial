fd80d0cf01069a1d3462c9b8b5e42862c8d4b1a1
YosephSE <88291669+YosephSE@users.noreply.github.com>
2025-02-19 01:33:03 +0300

    frontend initialized

---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-gradient-to-r from-green-600 to-green-400 text-white  text-9xl italic flex items-center justify-center min-h-screen">
+        Hello World!
+      </div>
+    </>
+  );
+}
+
+export default App;
